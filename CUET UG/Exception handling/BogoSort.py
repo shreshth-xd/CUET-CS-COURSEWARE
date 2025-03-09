19b365daa1aac2d16248fb13d8ced0caaab5a299
shreshth-xd <shreshthkanchan777@gmail.com>
2025-03-09 23:37:06 +0530

    Made a program to check if Bogo sort can sort an array in specified number of shuffles

---
+++ CUET UG/Exception handling/BogoSort.py
@@ -0,0 +1,6 @@
+"""
+For those, who don't Bogo sort is also a sorting algorithm which basically takes an array and 
+keeps on shuffling it until the array is automatically sorted.
+"""
+
+import random
