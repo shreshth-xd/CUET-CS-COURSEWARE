4b8012bd979b49d9cf59c429278a3d3b73f2e161
shreshth-xd <shreshthkanchan777@gmail.com>
2025-03-10 00:50:44 +0530

    Understanding through the concept of exception handling if Bogo sort is actually usefull or not.

--- CUET UG/Exception handling/BogoSort.py
+++ CUET UG/Exception handling/BogoSort.py
@@ -16,13 +16,25 @@ def IsSorted(array):
     for i in range(0,n-1):
         if array[i]>array[i+1]:
             return False
-        return True
+    return True
 
 
 try:
     chances=int(input("How many chances(no. of shuffles) do you want to give to Bogo sort: "))
+    
     for i in range(chances):
-        random.shuffle(lst)    
-    sorted=IsSorted(lst)
-except not sorted:
+        random.shuffle(lst) 
+        if IsSorted(lst):
+            print(lst)
+            break  
+
+    sorted_=IsSorted(lst)
+    if sorted_==False:
+        raise Exception("Random shuffling didn't do it.")
+    
+except Exception:
     print("Bogo sort couldn't sort it in",chances,"shuffles.")
+else:
+    print("Sorted succesfully.")
+finally:
+    print("I hope you've understood if random actions does make an impact in this universe or not 🍃.")
