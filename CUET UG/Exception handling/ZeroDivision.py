3dabae677860c139b6fa2ac65ce5839a9879b234
shreshth-xd <shreshthkanchan777@gmail.com>
2025-03-09 23:22:42 +0530

    Pushed the most foundational use case of try-except blocks

--- CUET UG/Exception handling/ZeroDivision.py
+++ CUET UG/Exception handling/ZeroDivision.py
@@ -1,1 +1,8 @@
 # Presenting the most basic usage of try-except blocks out there
+# As we all know that it's illegal to divide a number by 0, both in Computer science and Mathematics
+
+try:
+    num=int(input("Enter a number: "))
+    print(num/0)
+except ZeroDivisionError:
+    print("Division by 0 is not permissable.")
