2f7b887270e2fe9581fcde2ad56a88d58ae5e51d
shreshth-xd <shreshthkanchan777@gmail.com>
2025-03-10 23:47:25 +0530

    Recursive program to generate an entire Hailstone sequence starting from a specified positive integer

--- CUET UG/Recursion/Hailstone.py
+++ CUET UG/Recursion/Hailstone.py
@@ -1,13 +1,16 @@
 def Hailstone(n):
-    if n>0:
-        if n%2==0:
-            print(n)
-            Hailstone(n/2)
-        else:
-            print(n)
-            Hailstone(3*n+1)
     if n==1:
         print(n)
         return
+    if n>0:
+        if n%2==0:
+            print(n,end=" ")
+            Hailstone(n//2)
+        else:
+            print(n,end=" ")
+            Hailstone(3*n+1)
     else:
         print("The Hailstone sequence begins with a positive integer, no negative integer is allowed.")
+
+n=int(input("Enter the number to start the Hailstone sequence: "))
+Hailstone(n)
