3a9b4efe9d8cd5f645e77607de091017d0ce045b
shreshth-xd <shreshthkanchan777@gmail.com>
2025-03-10 23:51:53 +0530

    Recursive program to generate an entire Hailstone sequence starting from a specified positive integer is ready.

--- CUET UG/Recursion/Hailstone.py
+++ CUET UG/Recursion/Hailstone.py
@@ -1,3 +1,11 @@
+"""
+The Hailstone sequence starts with a positive integer n and is proceeded by using two rules, if n is even
+then the next number on the sequence is n/2, if the number is odd then the next number on the sequence is
+3*n+1, and so on the process is repeated, however, we have to add the base case in this recursive function 
+for the case where the number is or gets deduced to 1 at any point of the sequence otherwise without this
+we would obtain an infinitely recurring sequence which is of course not we actually want.
+"""
+
 def Hailstone(n):
     if n==1:
         print(n)
