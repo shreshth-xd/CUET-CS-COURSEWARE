2f06f914faacbc96c2467c65edb055a4c7e5950e
shreshth-xd <shreshthkanchan777@gmail.com>
2025-03-13 22:58:20 +0530

    Tested the register functionality of this login system, everything working fine right now

--- CUET UG/Project/main.py
+++ CUET UG/Project/main.py
@@ -2,15 +2,11 @@ import json
 import bcrypt
 import re
 
-numbers="0123456789"
-letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-# validForusername="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
-# strong_password=r"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_/\*-\+!@#\$%\^&\*\()-={}[]\|:;\"'<>,\.\?"
+# Regex pattern to enusre that the password contains numbers and special symbols along with letters
 pattern = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*\W)[A-Za-z\d\W]{8,}$'
 
-def get_hash(password_,round=12):
-    password=password_
+
+def get_hash(password,round=12):
     # Generating a salt to be included in the hashed password 
     salt=bcrypt.gensalt(rounds=round)    
     password_bytes = password.encode('utf-8')
@@ -21,18 +17,31 @@ def register(username,password):
     try:
         # Only letters, numbers and underscores are allowed in username field
         # The username has to be unique        
+
+        
+        # The regex pattern "\w+" ensures that the username has only letters, numbers and underscores
+        # Since the fullmatch() and match() methods of re returns an object, I have typecasted it into a bool
+
+        with open ("test.json","r") as file:
+            existing_data = json.load(file)
+            for key in existing_data.keys():
+                if username==key:
+                    raise NameError
+            pass
+
         if bool(re.fullmatch(r"\w+",username))==False:
             raise ValueError
         if bool(re.match(pattern,password))==False:
             raise KeyError
         else:
-            hashed_password = get_hash(password).decode(encoding='utf-8')
-    
-            data= {
+
+            # Decoding the bytes returned by the getHash function, since JSON module does not allows
+            # bytes to be dumped into a JSON file
+            hashed_password = get_hash(password).decode(encoding='utf-8')  
+            data = {
                 "username":username,
                 "password":hashed_password
             }
-
             try:
                 with open ("test.json","r") as file:
                     existing_data = json.load(file)
@@ -40,7 +49,7 @@ def register(username,password):
                 existing_data={}
 
 
-            with open("test.json","a") as file:
+            with open("test.json","w") as file:
                 if username not in existing_data:
                     existing_data[username] = data
                     json.dump(existing_data,file,indent=4)
@@ -51,7 +60,9 @@ def register(username,password):
     except KeyError:
         print("Your password is not strong.")
         return "Invalid password"
-    
+    except NameError:
+        print("Username is not unavailable")
+        return "Unavailable username"  
 # def login(username,password):
 #     try:
 #     # Only letters, numbers and underscores are allowed in username field
@@ -79,7 +90,7 @@ while True:
             password=str(input("Enter your password: "))
             response = register(username,password)
 
-            if response=="Invalid username" or response=="Invalid password":
+            if response=="Invalid username" or response=="Invalid password" or response=="Unavailable username":
                 print("Try to register yourself again.")
             else:
                 break
