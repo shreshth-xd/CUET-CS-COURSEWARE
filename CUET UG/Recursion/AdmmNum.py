a2563ead4ad6f5d35b033e6e78e2123dc450db64
shreshth-xd <shreshthkanchan777@gmail.com>
2025-03-20 23:49:02 +0530

    Intial commit of this file.

---
+++ CUET UG/Recursion/AdmmNum.py
@@ -0,0 +1,28 @@
+"""
+In this program we have a list which contains admission numbers of n number of students sorted in ascending
+order, after which user serches for a student through his/her admission number and we'll look for it
+using recursive binary search.
+"""
+
+record = [01,02,03,04,05,06,07,08,09,10]
+number = int(input('Enter the admission number of your student: '))
+low=0
+high=len(record)-1
+def BinarySearch(low,high,item,array):
+    if low<=high:
+        mid = int((low+high)/2)
+        if array[mid]==item:
+            return 1
+        elif array[mid]<item:
+            low=mid+1
+            BinarySearch(low,high,item,array)
+        elif array[mid]>item:
+            high=mid-1
+            BinarySearch(low,high,item,array)
+    else:
+        return 0
+response=BinarySearch(low,high,number,record)
+if response==1:
+    print("Search successfull, we do have this admission number in our record.")
+else:
+    print("Search successfull, we do not have this admission number in our record.")
