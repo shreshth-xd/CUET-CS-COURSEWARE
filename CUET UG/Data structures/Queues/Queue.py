404cbe5be4742609be57469d2f3eda1875f9b877
shreshth-xd <shreshthkanchan777@gmail.com>
2025-03-24 17:17:00 +0530

    Implementing Queues in Python using lists.

---
+++ CUET UG/Data structures/Queues/Queue.py
@@ -0,0 +1,16 @@
+# Implementing Queues in Python using lists
+
+"""
+1. Enqueue
+2. Dequeue
+3. Peek
+4. Underflow
+"""
+
+queue=[]
+
+def isEmpty(q):
+    if q==[]:
+        return True
+    else:
+        return False
