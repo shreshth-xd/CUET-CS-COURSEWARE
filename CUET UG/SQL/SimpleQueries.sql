8fe8b21767e49499f6e1944f9aaf77d989b838f2
shreshth-xd <shreshthkanchan777@gmail.com>
2025-03-25 23:03:58 +0530

    Simple queries

--- CUET UG/SQL/SimpleQueries.sql
+++ CUET UG/SQL/SimpleQueries.sql
@@ -1,2 +1,5 @@
 -- Command to open a existing database:
-use animals;
+use database;
+
+-- Now creating a sample Table(a relation) in that database:
+CREATE TABLE Registered_users (Name char(20), Age int, Sex char(10));
