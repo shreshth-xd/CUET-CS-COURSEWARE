2d1c273ab79341d1d0bcc9ba4db975999f19da11
shreshth-xd <shreshthkanchan777@gmail.com>
2025-03-26 23:46:51 +0530

    Python script to manage the data of student record's binary file

--- CUET UG/File handling/Binary files/StudentRecord.py
+++ CUET UG/File handling/Binary files/StudentRecord.py
@@ -2,18 +2,27 @@
 import pickle
 
 while True:
-    name = str(input("Enter the student's name: "))
-    marks = int(input("Enter his/her marks: "))
-    rec={}
-    rec[name] = marks
+    print("1. Push data to the file")
+    print("2. Retrieve data from the file")
+    choice=int(input("Enter your choice: "))
 
-    ask=str(input("You've got more records? "))
-    if "yes" in ask.lower():
-        continue
-    elif "no" in ask.lower():
-        break
-    else:
-        print("Answer in yes or no.")
+    if choice==1:
+        name = str(input("Enter the student's name: "))
+        marks = int(input("Enter his/her marks: "))
+        rec={}
+        rec[name] = marks
 
-with open("StudentRecord.dat","rb+") as file:
-    pickle.dump(rec,file)
+        ask=str(input("You've got more records? "))
+        if "yes" in ask.lower():
+            continue
+        elif "no" in ask.lower():
+            break
+        else:
+            print("Answer in yes or no.")
+
+        with open("StudentRecord.dat","ab") as file:
+            pickle.dump(rec,file)
+    
+    elif choice==2:
+        with open("StudentRecord.dat","rb") as file:
+            data=pickle.load(file)
