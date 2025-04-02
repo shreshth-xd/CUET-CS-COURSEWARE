e02b7f0ee97fb44376463099862ff60107d19fe5
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-02 23:12:10 +0530

    Using recursion to find if a given number is prime or not.

--- CUET UG/Recursion/RecursivePrimeTest.py
+++ CUET UG/Recursion/RecursivePrimeTest.py
@@ -1,5 +1,11 @@
 # Using Recursion to find if a given number is a prime number or not
 
+"""
+Using the √n logic to find if the number is prime or not.
+According to this logic if a number does not have any factor less than √n then it will never have a factor
+between 2 and n-1, which means that the number will be a prime number in this case.
+
+"""
 def PrimeTest(n):
     if n==0 or n==1 or n<0:
         return 0
