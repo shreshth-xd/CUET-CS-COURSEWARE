e7c0c469fffb05669f5cf4ffc0b95db198197213
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-03 16:58:28 +0530

    DML commands

---
+++ CUET UG/SQL/DataManipulation.sql
@@ -0,0 +1,4 @@
+-- Manipulating data of a specified table using DML commands, mostly to update, insert or delete records from a table.
+
+-- To insert a new, single and independent record in the specified table:
+INSERT INTO registered_users(name,age,sex) VALUES('John Doe',25,'Male');
