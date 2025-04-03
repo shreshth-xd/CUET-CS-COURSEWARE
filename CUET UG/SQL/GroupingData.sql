e41c0821ed816b52453d83e55a233b910254f407
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-03 17:01:08 +0530

    Using GROUP BY clause

--- CUET UG/SQL/GroupingData.sql
+++ CUET UG/SQL/GroupingData.sql
@@ -1,3 +1,4 @@
 -- Organizing the data in the results of queries using GROUP BY clause
 
 -- To count the number of male and female users in the registered_users table
+SELECT sex, count(sex) FROM registered_users GROUP BY sex;
