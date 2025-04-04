d70a896528e5dbcb30d3816d774669d608da7cd4
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-04 19:47:12 +0530

    Grouping data using SQL

--- CUET UG/SQL/GroupingData.sql
+++ CUET UG/SQL/GroupingData.sql
@@ -3,3 +3,5 @@
 -- To count the number of male and female users in the registered_users table
 SELECT sex, count(sex) FROM registered_users GROUP BY sex;
 
+-- To count the number of male and female users who are older than 18 years
+SELECT sex, count(sex) FROM registered_users GROUP BY sex HAVING age>18;
