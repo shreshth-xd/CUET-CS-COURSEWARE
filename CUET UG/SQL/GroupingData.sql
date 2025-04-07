33c8913e81e25c3ff60dc7c12b2d57e7bdee4982
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-07 22:55:45 +0530

    Retrieving data in groups from a specified table using SQL

--- CUET UG/SQL/GroupingData.sql
+++ CUET UG/SQL/GroupingData.sql
@@ -5,3 +5,8 @@ SELECT sex, count(sex) FROM registered_users GROUP BY sex;
 
 -- To count the number of male and female users who are older than 18 years
 SELECT sex, count(sex) FROM registered_users GROUP BY sex HAVING age>18;
+
+
+-- To calulcate the average age group of male and female users and sorting the order in descending order and making sure that there are
+-- more than 2 users in each gender
+SELECT sex, avg(age) FROM registered_users GROUP BY sex HAVING count(age)>2 ORDER BY avg(age) DESC;
