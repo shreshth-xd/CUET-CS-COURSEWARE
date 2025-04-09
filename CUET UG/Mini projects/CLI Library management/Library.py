9a4b2625e885885ef4fbf18bbff146fcad9e80fb
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-09 17:07:33 +0530

    Holly molly, fixed a major blunder in my code.

--- CUET UG/Mini projects/CLI Library management/Library.py
+++ CUET UG/Mini projects/CLI Library management/Library.py
@@ -256,7 +256,7 @@ while True:
                     print("Fill the information below to make your account.")
                     plan_=str(input("Select a plan: "))
                     profession=str(input("Enter your profession here (Student if none): "))
-                    SignUp(username,user,subscribed="No",plan=plan_,tenure_of_plan="Unlimited",Penalty=("None"),Profession=profession)
+                    SignUp(username,user,subscribed="No",plan=plan_,tenure_of_plan="Unlimited",Profession=profession)
             
                 duration=int(input("For how many days do you want to keep this book with yourself? "))
                 fine="150$"
@@ -288,7 +288,7 @@ while True:
                     print("Fill the information below to make your account.")
                     plan_=str(input("Select a plan: "))
                     profession=str(input("Enter your profession here (Student if none): "))
-                    SignUp(username,user,subscribed="No",plan=plan_,tenure_of_plan="Unlimited",Penalty=("None"),Profession=profession)
+                    SignUp(username,user,subscribed="No",plan=plan_,tenure_of_plan="Unlimited",Profession=profession)
 
                 ReturnStatus="N/A"
 
