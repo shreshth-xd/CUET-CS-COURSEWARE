8c2b45bc2bc0683dd9bf4cfcb54951994de828b0
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-11 13:16:15 +0530

    Implementing Deque in Python using lists

---
+++ CUET UG/Data structures/Queues/Deque.py
@@ -0,0 +1,27 @@
+# A Deque stands for double ending queue, which allows the insertion and deletion operation from both the ends
+
+queue=[]
+front=0
+rear=len(queue)-1
+
+
+def isEmpty(queue):
+    if queue==[]:
+        return True
+    else:
+        return False
+    
+
+def enqueue(item,queue):
+    if isEmpty(queue):
+        front=rear=0
+        queue.append(item)
+    else:
+        queue.append(item)
+        rear=len(queue)-1
+
+
+def dequeue(queue):
+    if isEmpty(queue):
+        print("Underflow")
+
