107a217938e188134bf6e954524675f555d7380b
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-11 23:22:25 +0530

    Checking for pallindrome strings

--- CUET UG/Recursion/PallindromeString.py
+++ CUET UG/Recursion/PallindromeString.py
@@ -4,9 +4,13 @@ string=str(input("Enter the string: "))
 def PallindromeString(string):
     string = "".join(c.lower() for c in string if c.isalnum())
 
-    if string=="":
-        return True
-    elif len(string)==1:
-        return True
-    elif string[0]!=string[len(string)-1]:
-        return False
+    def checkForPallindrome(left=0,right=len(string)-1):
+        if string=="":
+            return True
+        elif len(string)==1:
+            return True
+        elif string[0]!=string[len(string)-1]:
+            return False
+        
+        return checkForPallindrome(left+1,right-1)
+    
