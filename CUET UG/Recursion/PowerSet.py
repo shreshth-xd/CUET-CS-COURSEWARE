62507aed12d5f58fe564f1431c130eb854894b15
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-12 23:59:05 +0530

    Generating all the subsets of a given string

--- CUET UG/Recursion/PowerSet.py
+++ CUET UG/Recursion/PowerSet.py
@@ -4,9 +4,9 @@ def PowerSetGenerator(string,ptr=0,combs=None):
     if combs is None:
         combs = set()
 
-    if ptr in combs:
+    if string[ptr] in combs:
         return PowerSetGenerator(string[ptr+1:],combs=combs)
         pass
     
     combs.add("")
-    combs.add(string[ptr])    
+    combs.add(string[ptr])
