d615d9278cd3aa143386f0e01e3a4b07ea28c64b
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-12 23:20:05 +0530

    To find the sum of digits of a given integer till one digit

--- CUET UG/Recursion/DigitSum.py
+++ CUET UG/Recursion/DigitSum.py
@@ -1,7 +1,7 @@
 # To find the sum of the digits of the number until it reduces to a single digit
 
 def SumOfDigits(num,sum=0):
-    if sum//10==0:
+    if num==0:
         return sum
     rem=num%10
     sum+=rem
