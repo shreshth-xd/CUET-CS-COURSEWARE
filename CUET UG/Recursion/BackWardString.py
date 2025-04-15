38fb2869571dffae5c27e6f5a76ad257e72a545b
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-15 16:09:22 +0530

    Printing a string backwards using recursion.

---
+++ CUET UG/Recursion/BackWardString.py
@@ -0,0 +1,14 @@
+# Using Recursion to print a string backwards to understand how nested call stacks in recursion actually works:
+
+string = str(input("Enter the string: "))
+
+def bkwrds(string, n=0):
+    if n==len(string):
+        return
+    
+    else:
+        bkwrds(string,n+1)
+        print(string[n], end="")
+        # return 
+
+bkwrds(string)
