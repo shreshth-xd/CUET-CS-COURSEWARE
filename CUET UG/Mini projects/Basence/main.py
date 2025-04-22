9f2bc504fc64ceae4dab6b14fc50d2743efa473f
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-22 12:56:24 +0530

    CLI interface for performance analysis

--- CUET UG/Mini projects/Basence/main.py
+++ CUET UG/Mini projects/Basence/main.py
@@ -15,9 +15,9 @@ def barGraphGenerator(x,y,XLABEL,YLABEL,title):
     # Getting current axis
     ax=plt.gca()
     # Setting locator to the x axis and y axis so that it shows the values coming between two discrete integers
+    
     ax.xaxis.set_major_locator(MultipleLocator(1))
-    ax.yaxis.set_major_locator(MultipleLocator(0.2))
-
+    
     plt.title(f"{title}")
     plt.xlabel(f"{XLABEL}")
     plt.ylabel(f"{YLABEL}")
@@ -45,9 +45,9 @@ def lineGraphGenerator(x,y,XLABEL,YLABEL,title):
     # Getting current axis
     ax=plt.gca()
     # Setting locator to the x axis and y axis so that it shows the values coming between two discrete integers
+    
     ax.xaxis.set_major_locator(MultipleLocator(1))
-    ax.yaxis.set_major_locator(MultipleLocator(0.2))
-
+    
     plt.title(f"{title}")
     plt.xlabel(f"{XLABEL}")
     plt.ylabel(f"{YLABEL}")
