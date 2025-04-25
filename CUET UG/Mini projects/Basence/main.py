ca5f3ad5285272e7473963808fd6c689c0ecb379
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-25 13:08:16 +0530

    CLI interface for performance analysis

--- CUET UG/Mini projects/Basence/main.py
+++ CUET UG/Mini projects/Basence/main.py
@@ -84,7 +84,6 @@ def lineGraphGenerator(x,y,XLABEL,YLABEL,title):
 def UpdateRecord(table):
     if ("pcm") in table.lower():
         name=str(input("Enter the name of the student: "))
-        print(name)
         class_=str(input("Enter the class: "))
         subject=str(input("Enter the subject: "))
         marks=int(input("Enter the new marks of this student: "))
