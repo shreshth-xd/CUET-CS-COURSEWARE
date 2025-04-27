46ea5cf39c2d5a83d4997571a81532fa5fcbca08
shreshth-xd <shreshthkanchan777@gmail.com>
2025-04-27 16:56:30 +0530

    To compare linear and binary search in case of sorting list of words

--- CUET UG/Algorithms/Searching/Iterative/ComparingWithWords.py
+++ CUET UG/Algorithms/Searching/Iterative/ComparingWithWords.py
@@ -1,1 +1,13 @@
 wordList=["Perfect", "Stupendous", "Wondrous", "Gorgeous", "Awesome", "Mirthful", "Fabulous", "Splendid", "Incredible", "Outstanding", "Propitious","Remarkable", "Stellar", "Unbelievable", "Super", "Amazing"]
+
+def selectionSort(array,length):
+    for i in range(0, length-1):
+        min_=array[i]
+        for j in range(i+1, length):
+            if array[j]<min_:
+                min_=j
+        
+        if min_!=i:
+            array[i],array[min_]=array[min_],array[i]
+    
+    return array
