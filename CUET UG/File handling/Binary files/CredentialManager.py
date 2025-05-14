c2480635c34cb0238658d4f90bb85b307747f8d9
shreshth-xd <shreshthkanchan777@gmail.com>
2025-05-14 22:03:23 +0530

    Retreiving passwords from the user

--- CUET UG/File handling/Binary files/CredentialManager.py
+++ CUET UG/File handling/Binary files/CredentialManager.py
@@ -1,6 +1,7 @@
 import pickle
 
 print("Simple script to store your credentials in an encrypted form in a binary file.")
+password=str(input("Enter your password: "))
 
 with open("credentials.dat","ab+"):
     print("")
