44653cb64141b38c2e474bda46e5563b9f2453ae
shreshth-xd <shreshthkanchan777@gmail.com>
2025-05-14 22:20:21 +0530

    Retreiving passwords from the user

--- CUET UG/File handling/Binary files/CredentialManager.py
+++ CUET UG/File handling/Binary files/CredentialManager.py
@@ -11,5 +11,5 @@ copy_password=str(input("Copy password to clipboard: "))
 if copy_password.lower()!="no":
     with open(r'credentials.dat',"rb") as file:
         password=pickle.load(file)
-        print(password)
+        pyperclip.copy(password)
         print("Password copied to clipboard.")
