adb602c04e3606bd4085aa4888ae2629a864a1b0
shreshth-xd <shreshthkanchan777@gmail.com>
2025-05-17 17:22:22 +0530

    Just wrote a script to read a simple CSV file

--- CUET UG/File handling/CSV files/CsvOperations.py
+++ CUET UG/File handling/CSV files/CsvOperations.py
@@ -1,1 +1,8 @@
 # To use a sample csv file to see how we can interact with CSV data from a Python script
+import csv
+
+# Since the CSV files are read row by row in CSV module, it's not necessary to use try-except or with clause to tackle the EOFError
+file=open("Sample.csv","r")
+reader=csv.reader(file,delimiter=",")
+for row in reader:
+    print()
