ab5f23f7fce9083597006c3b208f36769f767283
shreshth-xd <shreshthkanchan777@gmail.com>
2025-05-17 17:26:01 +0530

    To generate the tabular data without the headings

--- CUET UG/File handling/CSV files/CsvOperations.py
+++ CUET UG/File handling/CSV files/CsvOperations.py
@@ -4,5 +4,6 @@ import csv
 # Since the CSV files are read row by row in CSV module, it's not necessary to use try-except or with clause to tackle the EOFError
 file=open("Sample.csv","r")
 reader=csv.reader(file,delimiter=",")
-for row in reader:
-    print(row)
+rows=[row for row in reader]
+for i in range(1,len(rows)):
+    print(rows[i])
