718022ea58b1340ff3fbc10506c6cfbd221298dd
shreshth-xd <shreshthkanchan777@gmail.com>
2025-05-18 21:28:20 +0530

    Simple try-except code block program

--- CUET UG/Exception handling/try-except.py
+++ CUET UG/Exception handling/try-except.py
@@ -1,1 +1,7 @@
 # Basic program of try-except clause for exception handling
+n=int(input("Enter a number: "))
+try:
+    print("I will see if there is any error :)")
+    print(1/n)
+except ZeroDivisionError:
+    print("Division by 0 is undefined")
