325846114e55464b98a3fb14af86b36561581977
shreshth-xd <shreshthkanchan777@gmail.com>
2025-05-18 21:32:31 +0530

    Simple try-except-else-finally code block program

--- CUET UG/Exception handling/try-except-else-finally.py
+++ CUET UG/Exception handling/try-except-else-finally.py
@@ -1,1 +1,11 @@
 n=int(input("Enter a number: "))
+n1=int(input("Enter another number: "))
+try:
+    print("Oh boy, let's check for some errors.")
+    n/n1
+except ValueError:
+    print("Please don't enter a string when you are asked for a freaking number!!!!")
+except ZeroDivisionError:
+    print("That's undefined bro")
+else:
+    print("Congratulations you just did it without encountering any error.")
