d8298702583a9c7f59e557e342c756a894d7416f
shreshth-xd <shreshthkanchan777@gmail.com>
2025-05-21 17:54:28 +0530

    Basic configuration script to integrate the Python script with a live SQL database

--- CUET UG/PY-SQL CON/Setup.py
+++ CUET UG/PY-SQL CON/Setup.py
@@ -1,2 +1,5 @@
 import mysql.connector as connector
 
+con = connector.connect(host="localhost", user="root", password="password", database="test")
+if con.is_connected():
+    print("Connection with database successful")
