bec37431a38e6ce79641e5840112e39a3d04d10c
shreshth-xd <shreshthkanchan777@gmail.com>
2025-05-22 18:03:03 +0530

    Closing the connection to the db

--- CUET UG/PY-SQL CON/Setup.py
+++ CUET UG/PY-SQL CON/Setup.py
@@ -3,3 +3,5 @@ import mysql.connector as connector
 con = connector.connect(host="localhost", user="root", password="password", database="test")
 if con.is_connected():
     print("Connection with database successful")
+
+con.close()
